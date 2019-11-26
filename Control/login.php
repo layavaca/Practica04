cd6cc8c972c9ea84148248d2ae4a4716fdd9630f
layavaca <56559559+layavaca@users.noreply.github.com>
2019-11-26 04:00:48 -0500

    first commit

---
+++ Control/login.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include "../../config/conexionDB.php";
+
+$correo = isset($_GET['id']) ? trim($_GET['id']) : null;
+$passwrd = isset($_GET['pass']) ? trim($_GET['pass']) : null;
+$sql = "SELECT * from usuario WHERE usu_correo = '$correo' AND usu_password = MD5('$passwrd')";
+$result = $conexion->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while ($row = $result->fetch_assoc()) {
+        $_SESSION['isLogged']= TRUE;
+        if ($row['usu_rol'] === 'a') {
+            echo $row['usu_codigo'].'yes';
+        } else {
+            echo $row['usu_codigo'].'no';
+        }
+    }
+} else {
+    echo 0;
+    
+}
